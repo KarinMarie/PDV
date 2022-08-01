29612be041c85a5804b4e8cb07e6e13421904b85
Karin Marie <93265472+KarinMarie@users.noreply.github.com>
2022-07-31 23:19:50 -0300

    Add files via upload

---
+++ PDV/forms/Login.Designer.cs
@@ -0,0 +1,184 @@
+﻿
+namespace PDV
+{
+    partial class Login
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.metroLabel1 = new MetroFramework.Controls.MetroLabel();
+            this.txtUsuario = new MetroFramework.Controls.MetroTextBox();
+            this.txtSenha = new MetroFramework.Controls.MetroTextBox();
+            this.metroLabel2 = new MetroFramework.Controls.MetroLabel();
+            this.btnLogar = new MetroFramework.Controls.MetroButton();
+            this.lblAviso = new MetroFramework.Controls.MetroLabel();
+            this.SuspendLayout();
+            // 
+            // metroLabel1
+            // 
+            this.metroLabel1.AutoSize = true;
+            this.metroLabel1.FontSize = MetroFramework.MetroLabelSize.Tall;
+            this.metroLabel1.Location = new System.Drawing.Point(23, 87);
+            this.metroLabel1.Name = "metroLabel1";
+            this.metroLabel1.Size = new System.Drawing.Size(70, 25);
+            this.metroLabel1.TabIndex = 0;
+            this.metroLabel1.Text = "Usuário";
+            // 
+            // txtUsuario
+            // 
+            // 
+            // 
+            // 
+            this.txtUsuario.CustomButton.Image = null;
+            this.txtUsuario.CustomButton.Location = new System.Drawing.Point(249, 1);
+            this.txtUsuario.CustomButton.Name = "";
+            this.txtUsuario.CustomButton.Size = new System.Drawing.Size(27, 27);
+            this.txtUsuario.CustomButton.Style = MetroFramework.MetroColorStyle.Blue;
+            this.txtUsuario.CustomButton.TabIndex = 1;
+            this.txtUsuario.CustomButton.Theme = MetroFramework.MetroThemeStyle.Light;
+            this.txtUsuario.CustomButton.UseSelectable = true;
+            this.txtUsuario.CustomButton.Visible = false;
+            this.txtUsuario.FontSize = MetroFramework.MetroTextBoxSize.Tall;
+            this.txtUsuario.Lines = new string[0];
+            this.txtUsuario.Location = new System.Drawing.Point(23, 115);
+            this.txtUsuario.MaxLength = 32767;
+            this.txtUsuario.Name = "txtUsuario";
+            this.txtUsuario.PasswordChar = '\0';
+            this.txtUsuario.PromptText = "Insira seu usuário";
+            this.txtUsuario.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.txtUsuario.SelectedText = "";
+            this.txtUsuario.SelectionLength = 0;
+            this.txtUsuario.SelectionStart = 0;
+            this.txtUsuario.ShortcutsEnabled = true;
+            this.txtUsuario.Size = new System.Drawing.Size(277, 29);
+            this.txtUsuario.Style = MetroFramework.MetroColorStyle.Pink;
+            this.txtUsuario.TabIndex = 1;
+            this.txtUsuario.UseSelectable = true;
+            this.txtUsuario.WaterMark = "Insira seu usuário";
+            this.txtUsuario.WaterMarkColor = System.Drawing.Color.FromArgb(((int)(((byte)(109)))), ((int)(((byte)(109)))), ((int)(((byte)(109)))));
+            this.txtUsuario.WaterMarkFont = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Pixel);
+            // 
+            // txtSenha
+            // 
+            // 
+            // 
+            // 
+            this.txtSenha.CustomButton.Image = null;
+            this.txtSenha.CustomButton.Location = new System.Drawing.Point(249, 1);
+            this.txtSenha.CustomButton.Name = "";
+            this.txtSenha.CustomButton.Size = new System.Drawing.Size(27, 27);
+            this.txtSenha.CustomButton.Style = MetroFramework.MetroColorStyle.Blue;
+            this.txtSenha.CustomButton.TabIndex = 1;
+            this.txtSenha.CustomButton.Theme = MetroFramework.MetroThemeStyle.Light;
+            this.txtSenha.CustomButton.UseSelectable = true;
+            this.txtSenha.CustomButton.Visible = false;
+            this.txtSenha.FontSize = MetroFramework.MetroTextBoxSize.Tall;
+            this.txtSenha.Lines = new string[0];
+            this.txtSenha.Location = new System.Drawing.Point(23, 197);
+            this.txtSenha.MaxLength = 32767;
+            this.txtSenha.Name = "txtSenha";
+            this.txtSenha.PasswordChar = '\0';
+            this.txtSenha.PromptText = "Insira seu usuário";
+            this.txtSenha.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.txtSenha.SelectedText = "";
+            this.txtSenha.SelectionLength = 0;
+            this.txtSenha.SelectionStart = 0;
+            this.txtSenha.ShortcutsEnabled = true;
+            this.txtSenha.Size = new System.Drawing.Size(277, 29);
+            this.txtSenha.Style = MetroFramework.MetroColorStyle.Pink;
+            this.txtSenha.TabIndex = 3;
+            this.txtSenha.UseSelectable = true;
+            this.txtSenha.WaterMark = "Insira seu usuário";
+            this.txtSenha.WaterMarkColor = System.Drawing.Color.FromArgb(((int)(((byte)(109)))), ((int)(((byte)(109)))), ((int)(((byte)(109)))));
+            this.txtSenha.WaterMarkFont = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Pixel);
+            // 
+            // metroLabel2
+            // 
+            this.metroLabel2.AutoSize = true;
+            this.metroLabel2.FontSize = MetroFramework.MetroLabelSize.Tall;
+            this.metroLabel2.Location = new System.Drawing.Point(23, 169);
+            this.metroLabel2.Name = "metroLabel2";
+            this.metroLabel2.Size = new System.Drawing.Size(59, 25);
+            this.metroLabel2.TabIndex = 2;
+            this.metroLabel2.Text = "Senha";
+            // 
+            // btnLogar
+            // 
+            this.btnLogar.FontSize = MetroFramework.MetroButtonSize.Tall;
+            this.btnLogar.FontWeight = MetroFramework.MetroButtonWeight.Regular;
+            this.btnLogar.Location = new System.Drawing.Point(23, 261);
+            this.btnLogar.Name = "btnLogar";
+            this.btnLogar.Size = new System.Drawing.Size(277, 46);
+            this.btnLogar.Style = MetroFramework.MetroColorStyle.Pink;
+            this.btnLogar.TabIndex = 4;
+            this.btnLogar.Text = "LOGAR";
+            this.btnLogar.UseSelectable = true;
+            this.btnLogar.Click += new System.EventHandler(this.btnLogar_Click);
+            // 
+            // lblAviso
+            // 
+            this.lblAviso.AutoSize = true;
+            this.lblAviso.FontWeight = MetroFramework.MetroLabelWeight.Bold;
+            this.lblAviso.Location = new System.Drawing.Point(23, 330);
+            this.lblAviso.Name = "lblAviso";
+            this.lblAviso.Size = new System.Drawing.Size(0, 0);
+            this.lblAviso.Style = MetroFramework.MetroColorStyle.Red;
+            this.lblAviso.TabIndex = 5;
+            this.lblAviso.UseStyleColors = true;
+            // 
+            // Login
+            // 
+            this.AcceptButton = this.btnLogar;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(323, 371);
+            this.Controls.Add(this.lblAviso);
+            this.Controls.Add(this.btnLogar);
+            this.Controls.Add(this.txtSenha);
+            this.Controls.Add(this.metroLabel2);
+            this.Controls.Add(this.txtUsuario);
+            this.Controls.Add(this.metroLabel1);
+            this.Name = "Login";
+            this.Resizable = false;
+            this.ShadowType = MetroFramework.Forms.MetroFormShadowType.AeroShadow;
+            this.Style = MetroFramework.MetroColorStyle.Pink;
+            this.Text = "Log-in";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private MetroFramework.Controls.MetroLabel metroLabel1;
+        private MetroFramework.Controls.MetroTextBox txtUsuario;
+        private MetroFramework.Controls.MetroTextBox txtSenha;
+        private MetroFramework.Controls.MetroLabel metroLabel2;
+        private MetroFramework.Controls.MetroButton btnLogar;
+        private MetroFramework.Controls.MetroLabel lblAviso;
+    }
+}
+
---
+++ PDV/forms/Login.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PDV
+{
+    public partial class Login : MetroFramework.Forms.MetroForm
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLogar_Click(object sender, EventArgs e)
+        {
+            if(string.IsNullOrEmpty(txtUsuario.Text) || string.IsNullOrEmpty(txtSenha.Text))
+            {
+                lblAviso.Text = "Por favor, preencha todos os campos.";
+                return;
+            }
+        }
+    }
+}
